94f7a6647d2fbe13434bf3a631b5903407e8ea57
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-01-13 23:02:39 -0300

    asdasdasd

---
+++ lib/src/flutter_camera.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FlutterCamera extends StatefulWidget {
+  @override
+  _FlutterCameraState createState() => _FlutterCameraState();
+}
+
+class _FlutterCameraState extends State<FlutterCamera> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlutterCamera'),
+      ),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
